16adde3278b4378f6fa6970bc50d487f0c474028
YohanSaibb <ebookconta105@gmail.com>
2026-02-11 21:16:26 -0300

    feat: Initialize i18n with language detection and support for multiple locales.

--- src/i18n.ts
+++ src/i18n.ts
@@ -23,7 +23,7 @@ i18n
             escapeValue: false,
         },
         detection: {
-            order: ['navigator', 'htmlTag', 'path', 'subdomain'],
+            order: ['querystring', 'navigator', 'htmlTag', 'path', 'subdomain'],
             caches: ['localStorage', 'cookie'],
         },
     });
